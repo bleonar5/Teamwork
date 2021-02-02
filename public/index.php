8708a5ecd4713fefccc24f7e86a11c4049f8f031
catherine <catherine@Brians-MacBook-Air.local>
2021-02-02 07:23:09 -0500

    git

--- public/index.php
+++ public/index.php
@@ -7,6 +7,9 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+header('Access-Control-Allow-Origin: *');  
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
