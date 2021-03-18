484ec765c6ea55bc2bd7fc6332b0307219a7540b
Brian <bleonar5@u.rochester.edu>
2021-03-18 08:33:14 -0400

    'git'

--- resources/views/layouts/participants/participant-login.blade.php
+++ resources/views/layouts/participants/participant-login.blade.php
@@ -7,9 +7,17 @@
 <script>
 
   // Firefox 1.0+
+var isFirefox = typeof InstallTrigger !== 'undefined';
 
+// Safari 3.0+ "[object HTMLElementConstructor]" 
+var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+
+if (isFirefox || isSafari){
+    window.location.href = '/browser-error'
+  }
 
 $( document ).ready(function() {
+
   $("#sign-in").on("click", function(event) {
     event.preventDefault();
     if ($('#participant_id').val() != ""){
